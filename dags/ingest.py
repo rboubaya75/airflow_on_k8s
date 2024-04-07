626d10bb77861502f092e616c193d0b8a59d9eaa
BOUBAYA Rachid (Externe) <rachid.boubaya-ext@vc-partner.net>
2024-04-07 19:22:17 +0200

    dags

--- dags/ingest.py
+++ dags/ingest.py
@@ -1,5 +1,4 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator 
 from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from datetime import datetime
 
@@ -10,38 +9,19 @@ default_args = {
     'retries': 1,
 }
 
-dag = DAG(
+with DAG(
     'csv_to_minio',
     default_args=default_args,
     schedule_interval=None,  # Set your desired schedule here
     catchup=False,
-)
+) as dag:
 
-def upload_to_minio():
-    # Set your Minio connection ID (configured in Airflow)
-    minio_conn_id = 'minio_connection'
-
-    # Specify the local file path
-    local_csv_path = 'C:\\Users\\RachidBOUBAYA\\Airflow-Tuto\\image'
-
-    # Specify the Minio bucket name and object key
-    bucket_name = 'cnam'
-    object_key = 'kbRU29BGMlSc7a6z0m7g'
-
-    # Upload the local file to Minio
-    upload_task = LocalFilesystemToS3Operator(
+    upload_to_minio = LocalFilesystemToS3Operator(
         task_id='upload_to_minio',
-        filename=local_csv_path,
-        bucket_name=bucket_name,
-        aws_conn_id=minio_conn_id,
-        key=object_key,
-        dag=dag,
+        filename='C:/Users/RachidBOUBAYA/Airflow-Tuto/image',  # Modified to avoid backslash issues
+        bucket_name='cnam',
+        aws_conn_id='minio_connection',  # Ensure this connection ID is correctly set up in Airflow
+        s3_key='kbRU29BGMlSc7a6z0m7g',  # `s3_key` is the correct parameter name, not `key`
     )
 
-# Call the upload task
-upload_to_minio()
-
-
-
-
-
+# No need to call upload_to_minio() since defining tasks within the DAG context makes them executable by Airflow
