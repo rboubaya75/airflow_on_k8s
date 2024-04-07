47a1847e4a7be69b65fc56f9831fbb9be9799b0a
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2024-04-07 20:26:28 +0200

    Update ingest.py

--- dags/ingest.py
+++ dags/ingest.py
@@ -1,51 +1,20 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflow.operators.bash import BashOperator
-from datetime import datetime
-from random import randint
+import datetime
 
-def _choose_best_model(ti):
-  accuracies = ti.xcom_pull(task_ids=[
-    'training_model_A',
-    'training_model_B',
-    'training_model_C'
-  ])
-  if max(accuracies) > 8:
-    return 'is_accurate'
-  return 'is_inaccurate'
+from airflow import models
 
-def _training_model(model):
-  print(model)
-  return randint(1, 10)
+default_dag_args = {
+    # The start_date describes when a DAG is valid / can be run. Set this to a
+    # fixed point in time rather than dynamically, since it is evaluated every
+    # time a DAG is parsed. See:
+    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
+    "start_date": datetime.datetime(2024, 4, 7),
+}
 
-with DAG("my_dag",
-  start_date=datetime(2023, 1 ,1), 
-  schedule_interval='@daily', 
-  catchup=False):
-
-  training_model_tasks = [
-    PythonOperator(
-      task_id=f"training_model_{model_id}",
-      python_callable=_training_model,
-      op_kwargs={
-        "model": model_id
-      }
-    ) for model_id in ['A', 'B', 'C']
-  ]
-
-  choose_best_model = BranchPythonOperator(
-    task_id="choose_best_model",
-    python_callable=_choose_best_model
-  )
-
-  accurate = BashOperator(
-    task_id="is_accurate",
-    bash_command="echo 'accurate'"
-  )
-
-  inaccurate = BashOperator(
-    task_id="is_inaccurate",
-    bash_command=" echo 'inaccurate'"
-  )
-
-  training_model_tasks >> choose_best_model >> [is_accurate, is_inaccurate]
+# Define a DAG (directed acyclic graph) of tasks.
+# Any task you create within the context manager is automatically added to the
+# DAG object.
+with models.DAG(
+    "composer_sample_simple_greeting",
+    schedule_interval=datetime.timedelta(days=1),
+    default_args=default_dag_args,
+) as dag:
