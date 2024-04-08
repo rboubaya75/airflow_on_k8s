5b4a18ce80c9b53fd3be3d0bb767d23b1c8b5c23
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2024-04-08 12:00:43 +0200

    Update ingest.py

--- dags/ingest.py
+++ dags/ingest.py
@@ -1,36 +1,7 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-import os
-from minio import Minio
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
-def create_hello_world_file():
-    file_path = "/tmp/hello_world.txt"
-    with open(file_path, "w") as f:
-        f.write("Hello, World!")
-    return file_path
-
-def upload_to_minio(ti):
-    file_path = ti.xcom_pull(task_ids='create_file_task')
-    minio_client = Minio("https://20.216.209.138:443",
-                         access_key="H2kmFP7WmkKx2J4n5E6U",
-                         secret_key="yM0abgtmC5DDdQ8hDKEQNn_COwbUWur-kCWo-DEt",
-                         secure=True)
-    minio_client.fput_object("cnam", "hello_world.txt", file_path)
-
-with DAG("hello_world_to_minio",
-         start_date=datetime(2023, 1, 1),
-         schedule_interval="@daily",
-         catchup=False) as dag:
-
-    create_file_task = PythonOperator(
-        task_id="create_file_task",
-        python_callable=create_hello_world_file,
-    )
-
-    upload_file_task = PythonOperator(
-        task_id="upload_file_task",
-        python_callable=upload_to_minio,
-    )
-
-    create_file_task >> upload_file_task
+def test_minio_connection(bucket_name: str):
+    hook = S3Hook(aws_conn_id='your_minio_conn')
+    keys = hook.list_keys(bucket_name)
+    for key in keys:
+        print(f"Found key: {key}")
