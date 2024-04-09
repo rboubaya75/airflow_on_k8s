a1d410ee0bc0c00105fe9694ed221e26abb0ded1
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2024-04-09 18:14:36 +0200

    Update upload_minio.py

--- dags/upload_minio.py
+++ dags/upload_minio.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 def upload_to_minio():
     try:
-        client = Minio("https://20.19.131.179:9443",
+        client = Minio("https://minio.cnam.svc.cluster.local:443",
                        access_key="Rd6YQYQOzOB2f0T2",
                        secret_key="yyEKqqUdMAVURAoEk7jKqxKEd42RoOq6",
                        secure=True)
