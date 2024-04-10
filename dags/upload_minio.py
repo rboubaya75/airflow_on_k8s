6aed9a75844ab7efb27f382dcf08c951f97f0be6
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2024-04-10 12:39:32 +0200

    Update upload_minio.py

--- dags/upload_minio.py
+++ dags/upload_minio.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 import boto3
 from botocore.client import Config
+
+bucket_name = 'my-test-bucket2'
 
 def create_minio_bucket():
     s3_client = boto3.client(
@@ -14,7 +16,7 @@ def create_minio_bucket():
         verify=False  # This disables SSL certificate verification
     )
 
-    bucket_name = 'my-test-bucket'
+   
     s3_client.create_bucket(Bucket=bucket_name)
     print(f"Bucket '{bucket_name}' created")
 
@@ -27,14 +29,12 @@ def upload_file_to_bucket():
         config=Config(signature_version='s3v4'),
         verify=False
     )
-    
-    bucket_name = 'my-test-bucket'
     file_name = 'example.csv'
     s3_client.upload_file(file_name, bucket_name, file_name)
     print(f"'{file_name}' uploaded to bucket '{bucket_name}'")
 
 with DAG('minio_bucket_and_upload', start_date=datetime(2021, 1, 1),
-         schedule_interval='@once', catchup=False) as dag:
+         schedule='@once', catchup=False) as dag:  # Updated here
     
     create_bucket_task = PythonOperator(
         task_id='create_minio_bucket',
