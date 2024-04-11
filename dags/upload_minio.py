6e9f5ad5cac84198b853e51b3136f82292c5e00a
BOUBAYA Rachid <71382131+rboubaya75@users.noreply.github.com>
2024-04-11 15:38:37 +0200

    Update upload_minio.py

--- dags/upload_minio.py
+++ dags/upload_minio.py
@@ -34,25 +34,7 @@ def create_bucket_if_not_exists(client, bucket_name):
     else:
         print(f"Bucket '{bucket_name}' already exists.")
 
-def list_all_buckets(client):
-    # List all buckets
-    buckets = client.list_buckets()
-    print("Buckets:")
-    for bucket in buckets:
-        print(f"- {bucket.name}")
 
-def upload_file_to_bucket():
-    s3_client = boto3.client(
-        service_name='s3',
-        endpoint_url='https://20.19.131.164:443',
-        aws_access_key_id="Rd6YQYQOzOB2f0T2",
-        aws_secret_access_key='yyEKqqUdMAVURAoEk7jKqxKEd42RoOq6',
-        config=Config(signature_version='s3v4'),
-        verify=False
-    )
-    file_name = 'example.csv'
-    s3_client.upload_file(file_name, bucket_name, file_name)
-    print(f"'{file_name}' uploaded to bucket '{bucket_name}'")
 
 with DAG('minio_bucket_and_upload', start_date=datetime(2021, 1, 1),
          schedule='@once', catchup=False) as dag:  # Updated here
@@ -67,4 +49,4 @@ with DAG('minio_bucket_and_upload', start_date=datetime(2021, 1, 1),
         python_callable=upload_file_to_bucket,
     )
 
-create_bucket_task >> upload_file_task
+create_bucket_task 
